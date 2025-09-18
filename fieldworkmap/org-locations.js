beca170c837a266b856a5ba1c9f141b12b41f770
haijunli <lihaijun0629@gmail.com>
2025-09-18 18:13:53 -0400

    misc

---
+++ fieldworkmap/org-locations.js
@@ -0,0 +1,212 @@
+var addressPoints = [
+  [
+    "2023-07-31 - Greenville, Ohio, USA",
+    40.1023909,
+    -84.6332986
+  ],
+  [
+    "2023-08-01 - Indianapolis, Indiana, USA",
+    39.7683331,
+    -86.1583502
+  ],
+  [
+    "2023-08-02 - Cairo, Illinois, USA",
+    37.0057958,
+    -89.1772449
+  ],
+  [
+    "2023-08-03 - Olive Branch, Mississippi, USA",
+    34.9617605,
+    -89.8295315
+  ],
+  [
+    "2023-08-04 - Grenada, Mississippi, USA",
+    33.772401,
+    -89.7674875
+  ],
+  [
+    "2023-08-05 - Brewton, Alabama, USA",
+    31.1039239,
+    -87.0722655
+  ],
+  [
+    "2023-08-06 - Matthews, North Carolina, USA",
+    35.1159532,
+    -80.7224386
+  ],
+  [
+    "2023-08-07 - Roanoke Rapids, North Carolina, USA",
+    36.4558016,
+    -77.6573514
+  ],
+  [
+    "2024-06-05 - Dallas, Texas, USA",
+    32.7762719,
+    -96.7968559
+  ],
+  [
+    "2024-06-06 - Allen, Texas, USA",
+    33.1031744,
+    -96.6705503
+  ],
+  [
+    "2024-06-07 - Lawton, Oklahoma, USA",
+    34.6086854,
+    -98.3903305
+  ],
+  [
+    "2024-06-08 - Odessa, Texas, USA",
+    31.8457149,
+    -102.367687
+  ],
+  [
+    "2024-06-09 - Amarillo, Texas, USA",
+    35.20729,
+    -101.8371192
+  ],
+  [
+    "2024-06-10 - Limon, Colorado, USA",
+    39.2660453,
+    -103.6856688
+  ],
+  [
+    "2024-06-11 - Ogallala, Nebraska, USA",
+    41.1248328,
+    -101.7189583
+  ],
+  [
+    "2024-06-12 - Colby, Kansas, USA",
+    39.3959655,
+    -101.0526881
+  ],
+  [
+    "2024-06-13 - Dodge City, Kansas, USA",
+    37.7527982,
+    -100.0170787
+  ],
+  [
+    "2024-06-14 - Arlington, Texas, USA",
+    32.7355816,
+    -97.1071186
+  ],
+  [
+    "2024-08-11 - Chicago, Illinois, USA",
+    41.8755616,
+    -87.6244212
+  ],
+  [
+    "2024-08-11 - Madison, Wisconsin, USA",
+    43.074761,
+    -89.3837613
+  ],
+  [
+    "2024-08-12 - Rice Lake, Wisconsin, USA",
+    45.5027926,
+    -91.733423
+  ],
+  [
+    "2024-08-13 - Wahpeton, North Dakota, USA",
+    46.265879,
+    -96.608863
+  ],
+  [
+    "2024-08-14 - Ames, Iowa, USA",
+    42.0267567,
+    -93.6170448
+  ],
+  [
+    "2024-08-15 - Milbank, South Dakota, USA",
+    45.219131,
+    -96.635617
+  ],
+  [
+    "2024-08-16 - Blue Springs, Missouri, USA",
+    39.017316,
+    -94.282265
+  ],
+  [
+    "2024-08-17 - Coralville, Iowa, USA",
+    41.6759977,
+    -91.580705
+  ],
+  [
+    "2024-08-18 - Oskaloosa, Iowa, USA",
+    41.2951037,
+    -92.6448451
+  ],
+  [
+    "2024-08-19 - Danville, Illinois, USA",
+    40.125222,
+    -87.6304614
+  ],
+  [
+    "2024-08-20 - Des Plaines, Illinois, USA",
+    42.0415823,
+    -87.8873916
+  ],
+  [
+    "2025-07-27 - Sioux Falls, South Dakota, USA",
+    43.5476008,
+    -96.7293629
+  ],
+  [
+    "2025-07-28 - Aberdeen, South Dakota, USA",
+    45.4649805,
+    -98.487813
+  ],
+  [
+    "2025-07-29 - Dickinson, North Dakota, USA",
+    46.8791756,
+    -102.7896242
+  ],
+  [
+    "2025-07-30 - Deadwood, South Dakota, USA",
+    44.3743812,
+    -103.7290723
+  ],
+  [
+    "2025-07-31 - Miles City, Montana, USA",
+    46.4070219,
+    -105.847843
+  ],
+  [
+    "2025-08-01 - Great Falls, Montana, USA",
+    47.5048851,
+    -111.29189
+  ],
+  [
+    "2025-08-02 - Shelby, Montana, USA",
+    48.5054997,
+    -111.856872
+  ],
+  [
+    "2025-08-03 - Williston, North Dakota, USA",
+    48.1465457,
+    -103.621814
+  ],
+  [
+    "2025-08-04 - Harvey, North Dakota, USA",
+    47.769723,
+    -99.935404
+  ],
+  [
+    "2025-08-05 - West Fargo, North Dakota, USA",
+    46.874967,
+    -96.900362
+  ],
+  [
+    "2017-03 - Poyang Lake, Jiangxi, China",
+    29.1859199,
+    116.2046216
+  ],
+  [
+    "2016-08 - Taizhou Bay, Zhejiang, China",
+    28.6218348,
+    121.6060932
+  ],
+  [
+    "2014-09 - Poyang Lake, Jiangxi, China",
+    29.1859199,
+    116.2046216
+  ]
+];
